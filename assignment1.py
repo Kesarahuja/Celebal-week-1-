3fb7a1a1638df80c70b32cf5aa179360076326b6
Kesar <ahujakesar@gmail.com>
2025-06-08 16:29:40 +0530

    Updated triangle pattern logic

--- assignment1.py
+++ assignment1.py
@@ -12,11 +12,40 @@ lower_triangle(5)
 # ****
 # *****
 
+
+
 #LOWER TRIANGULAR 
 def inverted_upper_triangle(n):
     for i in range(n, 0, -1):
         for j in range(i):
             print('*', end=' ')
         print() 
+#for example
 n=5
 inverted_upper_triangle(n)
+
+#example output
+# * * * * * 
+# * * * * 
+# * * *
+# * *
+# *
+
+
+
+#PYRAMID
+def pyramid_pattern(n):
+    for i in range(n):
+        print(' ' * (n-i-1), end='')
+        print('* ' * (i+1))
+# for example
+if __name__ == "__main__":
+    rows = 5
+    pyramid_pattern(rows)
+
+#example output
+#     * 
+#    * * 
+#   * * *
+#  * * * *
+# * * * * *
